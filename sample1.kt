511a584e8984648de1b68f3ee85f6a95f496cbd1
tremr <tremr@mail.ru>
2021-11-20 00:37:37 +0300

    second approximation

--- sample1.kt
+++ sample1.kt
@@ -1,15 +1,13 @@
-fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
-	val map: MutableMap<Int, Int> = mutableMapOf()
-	for (i in list.indices) {
-		val k = map.getOrDefault(i, list[i])
-		map += i to k
+fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int>
+{
+	list.sort()
+	
+	for (element in list)
+	{
+		if (element.hasNext())
+			if (element + element.next() == number)
+				return pairOf(element, element.next())
 	}
-
-	for (i in 0 until map.size) {
-		val delta = number - map.getValue(i)
-		map.remove(i)
-		for (it in map.keys) if (map.getValue(it) == delta) return i to it
-	}
-
+	
 	return -1 to -1
 }
