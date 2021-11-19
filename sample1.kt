ae8ae0b3e3b2cbe615113daa2406aa19e09cc9ac
tremr <tremr@mail.ru>
2021-11-20 01:14:16 +0300

    third approximation

--- sample1.kt
+++ sample1.kt
@@ -1,12 +1,18 @@
 fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int>
 {
-	list.sort()
+	var sorted_list: List<Pair<Int, Int>> = listOf()	
+	for (i in list.indices)
+		sorted_list += pairOf(i, list[i])
+		
+	sorted_list.sortWith(compareBy(Pair::second))
 	
-	for (element in list)
+	for (i in sorted_list.size - 1)
 	{
-		if (element.hasNext())
-			if (element + element.next() == number)
-				return pairOf(element, element.next())
+		val first_value  = sorted_list[i]
+		val second_value = sorted_list[i + 1]
+		
+		if (fist_value + second_value == number)
+			return pairOf(fist_value, second_value)		
 	}
 	
 	return -1 to -1
